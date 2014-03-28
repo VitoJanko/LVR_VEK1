64052aa8e0805523e8d238d950e6d201b01aa3ec
bezlajk <bezlajk@gmail.com>
2014-03-28 12:39:19 +0100

    komentiran SAT v svojem programu

---
+++ sat_solver.py
@@ -0,0 +1,105 @@
+# -*- coding: cp1250 -*-
+from bool import *
+#===============================================================================
+
+#SAT solver
+
+#===============================================================================
+
+def SAT(cnf,spr,cs):
+    #SAT(cnf, spr, cs) je funkcija, ki reši SAT za podan slovar spremenljivk z že doloèenimi vrednostmi in seznam spremenljivk, ki še nimajo doloèenih vrednosti.
+    #spr=spremenljivke, cs=seznam spremenljivk, ki nimajo se vrednosti
+    found = True
+                                #Najprej rešimo izraze v cnf z eno spremenljivko z doloèeno vrednostjo ali z niè spremenljivkami.
+    while found: 
+        cnf = vstavi(cnf,spr,cs)
+        found =False
+        for formula in cnf:
+            if len(formula)==0:
+                return False
+            if len(formula)==1:
+                found = True
+                if formula[0][0]=='-':
+                    spr[formula[0][1:]]=False
+                    if formula[0][1:] in cs:
+                        cs.remove(formula[0][1:])
+                else:
+                    spr[formula[0]]=True
+                    if formula[0] in cs:
+                        cs.remove(formula[0])
+                        
+                                #Doloèimo vrednost spremenljivk, ki še nimajo doloèenih vrednosti. 
+    if len(cnf)==0:
+        return spr
+    else:
+        neReseno = cs[0]
+        cs = cs[1:]
+        spr[neReseno] = True
+        resitev = SAT(cnf, spr, cs)
+        if resitev:
+           return resitev
+        else:
+            spr[neReseno] = False
+            resitev = SAT(cnf, spr, cs)
+            if resitev:
+                return resitev
+            else:
+                return False
+        
+        
+
+def vstavi(cnf, spr, cd):
+    #VSTAVI(cnf,spr,cd) je funkcija, ki vrne poenostavljen cnf.
+    #spr=slover spremenljivk, cs=seznam spremenljivk, ki nimajo se vrednosti
+    sez=[]
+    for formula in cnf:
+        podsez=[]
+        isTrue = False
+                                    #Odstranimo notranje elemente, ki so False in vrne prazen seznam, ko najde element True.
+        for f in formula:
+            found = False
+            if f in spr.keys():             #spremenlivka je že v slovarju spr
+                if spr[f]:              
+                    found =  True
+                    isTrue = True
+                else:
+                    found= True
+            if f[1:] in spr.keys():         #spremenljivka brez negacije je že v slovarju spr
+                if spr[f[1:]]:
+                    found =  True
+                else:
+                    isTrue = True
+                    found= True
+            if not found:                   #podsez je seznam vseh spremenljivk, ki še nimajo doloèene vrednosti
+                podsez.append(f)
+        if not isTrue:                  
+            sez.append(podsez)
+    return sez
+    
+def resi(izraz):
+    #RESI(izraz) je funkcija, ki reši SAT s pomoèjo funkcije SAT(cnf,spr,cs), in vrne seznam vrednosti spremenljivk, za katere je izraz resnièen.
+    izraz = cnf(izraz)
+    cnf1 = [f.formula for f in izraz.formula]
+    cs = []
+    cnF = []
+                                #Sestavimo seznam vseh spremenljivk, ki v izrazu nastopajo.
+    for f in cnf1:
+        s=[]
+        for ff in f:
+            if isinstance(ff,Var):
+                s.append(ff.ime)
+                if ff.ime not in cs:
+                    cs.append(ff.ime)
+            elif isinstance(ff,Not):
+                if isinstance(ff.formula,Var):
+                    s.append('-'+ff.formula.ime)
+                    if ff.formula.ime not in cs:
+                        cs.append(ff.formula.ime)
+        cnF.append(s)
+    return SAT(cnF, {}, cs)
+            
+                
+            
+    
+        
+        
