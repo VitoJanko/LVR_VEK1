5e04af766830db5dd146d56bc29df90d3ca4d66a
bezlajk <bezlajk@gmail.com>
2014-04-30 20:14:52 +0200

    Poimenovani primeri(pocasi pa bo vse OK)

--- tester.py
+++ tester.py
@@ -2,28 +2,34 @@ from random import *
 from bool import *
 from sat_solver import *
 
-seznam = ["nnf", "nnf2", "spl", "cnf"]
+seznam = ["nnf", "cnf"]
 n = 0
-def Test(izraz):
-    exec("c="+izraz)
-    print str(izraz)
-    print "Poenostavljeno: "+str(simplify(c))
-    print "V cnf obliki:   "+str(cnf(c))
-    
-    for i in range(len(seznam)):
-        exec("b="+izraz+"."+seznam[i]+"()")
-        print seznam[i]+": "+str(b)
-    print resi(b)
+def Test(izraz_in):
+    izraz=izraz_in.split(":")
+    exec("c="+izraz[1])
+    print str(izraz[0])
+    print str(izraz[1])
     print
+    for i in range(len(seznam)):
+        exec("b="+izraz[1]+"."+seznam[i]+"()")
+        print seznam[i].upper()+": "+str(b)
+    print "Poenostavljena CNF oblika:   "+str(cnf(c))
+    print "Poenostavljeno: "+str(simplify(c)) 
+    print "Resitev SAT-a: ",resi(b)
+    
+    print "______________________________________________________________________"
 
 f=open("primeri.txt",'r')
 primeri = []
 for line in f:
-    if line!="\n":
+    if line.find(":")>0 and line.split(":")[0]!="Primer":
+        Test(line.strip("\n"))
+    if line.split(":")[0]=="Primer":
         primeri.append(line.strip("\n"))
         n+=1
-        Test(line.strip("\n"))
-    
+        
+
+#Test("And([Not(Var('p')), Var('p'),Var('q')])")  
 m = randint(0, n)
-Test(primeri[m])
+Test(primeri[-1])
 
